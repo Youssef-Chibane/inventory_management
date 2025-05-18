d99b33206851c6a19e08347cc97bcb5d768f7150
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-05-18 08:45:39 +0100

    no styles home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center mt-12 gap-4">
-      <h1 className="text-4xl font-bold">Home Page</h1>
+    <div>
+      <h1 className="text-2xl font-bold">Home Page</h1>
     </div>
   );
 }
