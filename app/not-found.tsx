ad63bec26afe8ea2a5e76e2a6f72553ca4ad2888
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-05-18 18:53:52 +0100

    text color

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div className="flex flex-col justify-center items-center mt-20 gap-4">
-      <h2 className="text-4xl font-bold">Not Found</h2>
+      <h2 className="text-4xl font-bold text-red-500">Not Found</h2>
       <p>Could not find requested resource</p>
       <Button asChild>
         <Link href="/">Return Home</Link>
