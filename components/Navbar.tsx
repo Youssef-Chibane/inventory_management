d8c9a2a14470ded44b9f57ef01e7f761e88edc43
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-05-18 19:35:31 +0100

    navbar done?

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,13 +4,11 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "./ModeToggle";
 import AuthButton from "@/components/authButton";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { Menu } from "lucide-react";
 
 const Navbar = () => {
   return (
     <nav className="border-b py-4">
-      <div className="container mx-auto flex items-center justify-between">
+      <div className="container mx-auto hidden lg:flex items-center justify-between">
         <Link href="/" className="text-2xl font-bold">
           Inventory Management
         </Link>
@@ -29,39 +27,24 @@ const Navbar = () => {
           <AuthButton />
           <ThemeToggle />
         </div>
+      </div>
 
-        {/* Mobile Navigation */}
-        <div className="md:hidden flex items-center">
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button variant="ghost" size="icon">
-                <Menu className="h-5 w-5" />
-                <span className="sr-only">Open menu</span>
-              </Button>
-            </SheetTrigger>
-            <SheetContent side="right" className="w-[250px]">
-              <div className="flex flex-col gap-4 mt-8">
-                <Link
-                  href="/"
-                  className="text-lg font-medium hover:text-primary transition-colors"
-                >
-                  Home
-                </Link>
-                <Link
-                  href="/dashboard"
-                  className="text-lg font-medium hover:text-primary transition-colors"
-                >
-                  Dashboard
-                </Link>
-                <div className="mt-2">
-                  <AuthButton />
-                </div>
-                <div className="mt-2">
-                  <ThemeToggle />
-                </div>
-              </div>
-            </SheetContent>
-          </Sheet>
+      {/* Mobile Navigation */}
+      <div className="container mx-auto lg:hidden flex  items-center justify-between px-4">
+        <div className="flex items-center gap-6">
+          <Link href="/" className="hover:text-primary transition-colors">
+            Home
+          </Link>
+          <Link
+            href="/dashboard"
+            className="hover:text-primary transition-colors"
+          >
+            Dashboard
+          </Link>
+        </div>
+        <div className="flex items-center gap-6">
+          <AuthButton />
+          <ThemeToggle />
         </div>
       </div>
     </nav>
