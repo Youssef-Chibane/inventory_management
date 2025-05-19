170c5607c13d93f06d2baaa4a72dda2b41ef09d4
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-05-19 23:20:48 +0100

    deleted border bottom

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,14 @@
-"use client";
-
 import Link from "next/link";
 import { ThemeToggle } from "./ModeToggle";
 
 const Navbar = () => {
   return (
-    <nav className="border-b py-4">
-      <div className="container mx-auto hidden lg:flex items-center justify-between">
-        <Link href="/" className="text-2xl font-bold">
+    <nav>
+      <div className="flex justify-between items-center py-4">
+        <Link href={"/"} className="text-2xl font-bold">
           Inventory Management
         </Link>
 
-        {/* Desktop Navigation */}
         <div className="hidden md:flex items-center gap-6">
           <Link href="/" className="hover:text-primary transition-colors">
             Home
@@ -22,24 +19,6 @@ const Navbar = () => {
           >
             Dashboard
           </Link>
-          <ThemeToggle />
-        </div>
-      </div>
-
-      {/* Mobile Navigation */}
-      <div className="container mx-auto lg:hidden flex  items-center justify-between px-4">
-        <div className="flex items-center gap-6">
-          <Link href="/" className="hover:text-primary transition-colors">
-            Home
-          </Link>
-          <Link
-            href="/dashboard"
-            className="hover:text-primary transition-colors"
-          >
-            Dashboard
-          </Link>
-        </div>
-        <div className="flex items-center gap-6">
           <ThemeToggle />
         </div>
       </div>
